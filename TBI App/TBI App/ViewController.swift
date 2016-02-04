15e0ac92c7532a542020bf5a3a6cebdaebf3c221
George McDonnell <g.a.mcdonnell7@gmail.com>
2016-02-04 12:46:56 +0000

    Added some basic navigation controls, mimicking research kit design

--- TBI App/TBI App/ViewController.swift
+++ TBI App/TBI App/ViewController.swift
@@ -11,17 +11,33 @@ import UIKit
 class ViewController: UIViewController, UIPageViewControllerDataSource {
     
     var pageViewController: UIPageViewController?
+    var currentIndex = 0
     @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    @IBOutlet weak var backArrow: UIImageView!
+    @IBOutlet weak var backButton: UIButton!
     
     var qVcs = [QuestionViewController]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        nextButton.layer.borderColor = nextButton.tintColor!.CGColor
+        nextButton.layer.borderWidth = 1
+        nextButton.layer.cornerRadius = 5
+        nextButton.layer.masksToBounds = true
+        
+        // Disable swipe to scroll
+        for view in pageViewController!.view.subviews {
+            if (view.isKindOfClass(UIScrollView)) {
+                (view as! UIScrollView).scrollEnabled = false
+            }
+        }
     }
     
     func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
-        let currentIndex = qVcs.indexOf(viewController as! QuestionViewController)!
+        currentIndex = qVcs.indexOf(viewController as! QuestionViewController)!
         
         if (currentIndex == 0) {
             return nil;
@@ -32,7 +48,7 @@ class ViewController: UIViewController, UIPageViewControllerDataSource {
     }
     
     func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
-        let currentIndex = qVcs.indexOf(viewController as! QuestionViewController)!
+        currentIndex = qVcs.indexOf(viewController as! QuestionViewController)!
         
         if (currentIndex == qVcs.count - 1) {
             return nil;
@@ -78,6 +94,31 @@ class ViewController: UIViewController, UIPageViewControllerDataSource {
             self.pageViewController!.dataSource = self;
 
             self.pageViewController?.setViewControllers([qVcs.first!], direction: UIPageViewControllerNavigationDirection.Forward, animated: false, completion: nil)
+        }
+    }
+    
+    
+    @IBAction func nextQuestion() {
+        if (currentIndex != qVcs.count - 1) {
+            let nextIndex = abs((currentIndex + 1) % qVcs.count)
+            pageViewController?.setViewControllers([qVcs[nextIndex]], direction: UIPageViewControllerNavigationDirection.Forward, animated: true, completion: nil)
+            currentIndex = nextIndex
+            
+            backArrow.hidden = false
+            backButton.enabled = true
+        }
+    }
+    
+    @IBAction func previousQuestion() {
+        if (currentIndex != 0) {
+            let previousIndex = abs((currentIndex - 1) % qVcs.count)
+            pageViewController?.setViewControllers([qVcs[previousIndex]], direction: UIPageViewControllerNavigationDirection.Reverse, animated: true, completion: nil)
+            currentIndex = previousIndex
+            
+            if (currentIndex == 0) {
+                backArrow.hidden = true
+                backButton.enabled = false
+            }
         }
     }
 }
